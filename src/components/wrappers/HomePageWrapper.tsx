323de48121ac0379daeff8e4a8b7287def6f31a1
Avinashsingh96 <singhgautamavinash@gmail.com>
2025-07-26 17:24:55 +0530

    fixed build issue

--- src/components/wrappers/HomePageWrapper.tsx
+++ src/components/wrappers/HomePageWrapper.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+
 import { useAppDispatch } from '@/store/hooks';
 import { addCustomCategory } from '@/store/slices/budgetSlice';
 import { Category } from '@/types';
